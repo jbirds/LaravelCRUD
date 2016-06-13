5c55d2a6e407c80f17431dc9b4b56fb8ffd1ca0e
Jason Wu <me@jwu.io>
2016-06-13 15:12:35 -0700

    Disabled debug mode.

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 6/13/16
+ * Time: 3:09 PM
+ */
---
+++ resources/views/haven/error.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 6/13/16
+ * Time: 2:54 PM
+ */
